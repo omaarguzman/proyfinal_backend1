9379419ad5b807775fbbcd9a246990992013d52c
Omar Guzmán <jogr.guzman@gmail.com>
2024-10-20 23:42:38 -0600

    Agregando productos en tiempo real

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -12,7 +12,35 @@ router.get('/realTimeProducts', (req, res) => {
 });
 
 router.post('/realTimeProducts', (req, res) => {
+  const { name, price, description } = req.body;
+  const maxId = Math.max(...Products.map(product => +product.id));
+  const newProduct = { id: maxId + 1, name, price, description };
+  Products.push(newProduct);
   res.status(200).render('realTimeProducts', { Products });
+});
+
+router.put('/realTimeProducts/:id', (req, res) => {
+  const { id } = parseInt(req.params.id);
+  const index = Products.findIndex(product => product.id === +id);
+
+  if (index > -1) {
+    Products[index] = req.body;
+    res.status(200).render('realTimeProducts', { Products });
+  } else {
+    res.status(404).send({ error: 'Producto no encontrado' });
+  }
+});
+
+router.delete('/realTimeProducts/:id', (req, res) => {
+  const { id } = parseInt(req.params.id);
+  const index = Products.findIndex(product => product.id === +id);
+
+  if (index > -1) {
+    Products.splice(index, 1);
+    res.status(200).render('realTimeProducts', { Products });
+  } else {
+    res.status(404).send({ error: 'Producto no encontrado' });
+  }
 });
 
 export default router;
